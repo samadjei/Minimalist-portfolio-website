1210e94c935b68f105cf116d1e16eff77446731c
Samuel Adjei <info.samadjei@gmail.com>
2021-08-17 18:39:33 +0100

    added responsive navbar

--- components/layout.js
+++ components/layout.js
@@ -7,11 +7,13 @@ const Layout = ({ children }) => {
 			<Head>
 				<title>Frontend Mentor | Minimalist Portfolio</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png" />
-
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				{/* Google Font  */}
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 				<link href="https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova:wght@400;700&family=Public+Sans&display=swap" rel="stylesheet" />
+				{/* Font Awesome */}
+				<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"></link>
 			</Head>
 			<div>
 				<Navbar />
--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Component } from 'react';
 
-const Navbar = () => {
-	return (
-		<div>
-			<p>This is a navbar</p>
-		</div>
-	);
-};
+class Navbar extends Component {
+	state = { clicked: false };
+	handleClick = () => {
+		this.setState({ clicked: !this.state.clicked });
+	};
+	render() {
+		return (
+			<header className="navbar">
+				<div className="navbar__logo">
+					<Link href="/">
+						<Image src="/logo.svg" alt="Minimalist logo" width={60.08} height={32} />
+					</Link>
+				</div>
+				<div onClick={this.handleClick}>
+					<i className={this.state.clicked ? 'fas fa-times active' : 'fas fa-bars'}></i>
+				</div>
+				<ul className={this.state.clicked ? 'navbar__links active' : 'navbar__links'}>
+					<Link href="/">
+						<a className="navbar--items">Home</a>
+					</Link>
+					<Link href="/portfolio">
+						<a className="navbar--items">Portfolio</a>
+					</Link>
+					<Link href="/contact">
+						<a className="navbar--items">Contact Me</a>
+					</Link>
+				</ul>
+			</header>
+		);
+	}
+}
 
 export default Navbar;
